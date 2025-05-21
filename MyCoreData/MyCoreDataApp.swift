7da29d5dcb86e1d472e007aeedfd27ff04810032
mncnf <mncnf0113@gmail.com>
2025-05-21 22:09:32 +0900

    Initial Commit

---
+++ MyCoreData/MyCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreDataApp.swift
+//  MyCoreData
+//
+//  Created by Development on 2025/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
